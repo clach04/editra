6ed917c13992ad9afbd03d705d64e5532a8201a1
clach04 <clach04@gmail.com>
2019-11-27 12:30:13 -0800

    _winrecycle.py that allows Editra to start
    Does not actually do anything other than allow Editra to start.
    Does NOT contain pre-compiled binaries.

---
+++ src/ebmlib/_winrecycle.py
@@ -0,0 +1,5 @@
+# Looking at https://github.com/wxWidgets/wxPython/blob/master/wx/tools/Editra/src/ebmlib/_winrecycle.py
+# do NOT want to have this due to the use of embedded exe/binary files
+# TODO check out https://github.com/wxWidgets/wxPython/tree/master/wx/tools/Editra
+
+recycle = 'yeah... we are not going to do that. see https://github.com/wxWidgets/wxPython/blob/master/wx/tools/Editra/src/ebmlib/_winrecycle.py'
